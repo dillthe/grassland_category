60a27b9f698662cc7927b3bed18365856a7ab75b
dillthe <seulgi0908@gmail.com>
2025-03-06 02:09:07 -0500

    feat: Updated Keyword DTO to improve code rreadability and efficiency

--- src/main/java/com/github/category/web/dto/KeywordDTO.java
+++ src/main/java/com/github/category/web/dto/KeywordDTO.java
@@ -10,6 +10,7 @@ import lombok.*;
 public class KeywordDTO {
     private int keywordId;
     private int categoryId;
-    private String categoryName;
+    //키워드 전체 조회 시 카테고리 명도 불러오고 싶은 경우 사용 - 키워드 중복 있는지 확인할 때 사용하기 좋음
+//    private String categoryName;
     private String keyword;
 }
